16464eb99211956cb0f92403af86797a655f278b
sudiptaVSE <71029731+sudiptaVSE@users.noreply.github.com>
2020-10-22 12:07:51 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,24 +4,24 @@ using Test
 @testset "NewtonsMethod.jl" begin
    testf(x)= (x-1)^3
     testf_prime(x)=3*(x-1)^2
-    testf1(x)= exp(x)
-    testf1_prime(x)=exp(x)
+    testf1(x)= exp(3*x)-1
+    testf1_prime(x)=3*exp(3*x)
     
     #test for functions with derivative
 
     root1, conv, iter  = NewtonsMethod.newtonroot(testf,testf_prime, 0.0)
-    #root2, conv, iter  = NewtonsMethod.newtonroot(testf1,testf1_prime, 1.0)
+    root2, conv, iter  = NewtonsMethod.newtonroot(testf1,testf1_prime, 0.0)
     
     @test abs(root1 - 1.0)<1e-6
-   # @test abs(root2 - 0.0)<1e-6
+    @test abs(root2 - 0.0)<1e-6
     
     #do the same for functions without derivative
 
     root1, conv, iter  = NewtonsMethod.newtonroot(testf, 0.0)
-   # root2, conv, iter  = NewtonsMethod.newtonroot(testf1, 1.0)
+    root2, conv, iter  = NewtonsMethod.newtonroot(testf1, 0.0)
     
     @test abs(root1 - 1.0) < 1e-6
-    #@test abs(root2 - 0.0) < 1e-6
+    @test abs(root2 - 0.0) < 1e-6
     
 
     #check maxiter works
