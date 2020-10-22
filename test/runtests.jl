1fa0cd1c9fdc1e54cce9d2ecea2f9e5a09d92d24
SudiptaVSE <sudipta.ghosh@ubc.ca>
2020-10-22 00:03:23 -0700

    changed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,43 +2,43 @@ using NewtonsMethod
 using Test
 
 @testset "NewtonsMethod.jl" begin
-   testfn(x)= (x-1)^3
-    testfn_prime(x)=3*(x-1)^2
-    testfn1(x)= exp(x)-1
-    testfn_prime1(x)=exp(x)
-    testfn2(x)= atan(x)
-    testfn_prime2(x)= 1/((x)^2+1) 
+   testf(x)= (x-1)^4
+    testf_prime(x)=4*(x-1)^3
+    testf1(x)= exp(3*x)-1
+    testf1_prime(x)=3*exp(x)
+    testf2(x)= atan(x)
+    testf2_prime(x)= 1/((x)^2+1) 
 
     #test for functions with derivative
 
-    root1, conv, iter  = NewtonsMethod.newtonroot(testfn,testfn_prime,x_0=0.0)
-    root2, conv, iter  = NewtonsMethod.newtonroot(testfn1,testfn_prime1,x_0=1.0)
-    root3, conv, iter  = NewtonsMethod.newtonroot(testfn1,testfn_prime1,x_0=-0.3)
+    root1, conv, iter  = NewtonsMethod.newtonroot(testf,testf_prime,x_0=0.0)
+    root2, conv, iter  = NewtonsMethod.newtonroot(testf1,testf1_prime,x_0=1.0)
+    root3, conv, iter  = NewtonsMethod.newtonroot(testf2,testf2_prime,x_0=-0.3)
 
-    @test abs(root1-1.0)<1e-6
+    @test abs(root1 - 1.0)<1e-6
     @test abs(root2 - 0.0)<1e-6
     @test abs(root3 - 0.0)<1e-6
 
     #do the same for functions without derivative
 
-    root1, conv, iter  = NewtonsMethod.newtonroot(testfn,x_0=0.0)
-    root2, conv, iter  = NewtonsMethod.newtonroot(testfn1,x_0=1.0)
-    root3, conv, iter  = NewtonsMethod.newtonroot(testfn1,x_0=-.4)
+    root1, conv, iter  = NewtonsMethod.newtonroot(testf,x_0=0.0)
+    root2, conv, iter  = NewtonsMethod.newtonroot(testf1,x_0=1.0)
+    root3, conv, iter  = NewtonsMethod.newtonroot(testf2,x_0=-.4)
 
-    @test abs(root1-1.0)<1e-6
+    @test abs(root1 - 1.0)<1e-6
     @test abs(root2 - 0.0)<1e-6
     @test abs(root3 - 0.0)<1e-6
 
 
     #check maxiter works
-    root1, conv, iter  = NewtonsMethod.newtonroot(testfn,testfn_prime,x_0=0.5,  maxiter = 3)
+    root1, conv, iter  = NewtonsMethod.newtonroot(testf,testf_prime,x_0=0.5,  maxiter = 3)
     # if the function converged, return number of iteration, else return nothing 
     @test iter == nothing ||iter <= 3
     #check for tolerance, increasing it to 1e-8
-    root1, conv, iter  = NewtonsMethod.newtonroot(testfn,testfn_prime,x_0=0.5,  tol = 1e-8)
+    root1, conv, iter  = NewtonsMethod.newtonroot(testf,testf_prime,x_0=0.5,  tol = 1e-8)
     @test abs(root1-1.0)<1e-7
     
-    #check lack iof convergence
+    #check lack of convergence
     
     test_nc(x) = x^2+1
 
