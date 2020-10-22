5db0550372f8a8e0b7c960e86242900d0e7cdbe5
sudiptaVSE <71029731+sudiptaVSE@users.noreply.github.com>
2020-10-22 01:36:33 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,33 +4,33 @@ using Test, Polynomials
 @testset "NewtonsMethod.jl" begin
     testf(x) = (x-1)^2
     testf_prime(x) = 2*(x-1)
-    testf1(x) = exp(3*x)-1
-    testf1_prime(x) = 3*exp(3*x)
+    testf1(x) = exp(3*x)-2*x
+    testf1_prime(x) = 3*exp(3*x)-2
     
     #test for functions with derivative
 
     root1, conv, iter  = NewtonsMethod.newtonroot(testf, testf_prime, 0.0)
     root2, conv, iter  = NewtonsMethod.newtonroot(testf1, testf1_prime, 1.0)
     
-    @test abs(root1 - 1.0)<1e-6
-    @test abs(root2 - 0.0)<1e-6
+    @test abs(root1 - 1.0) < 1e-6
+    @test abs(root2 - 0.0) < 1e-6
     
     #do the same for functions without derivative
 
     root1, conv, iter  = NewtonsMethod.newtonroot(testf, 0.0)
     root2, conv, iter  = NewtonsMethod.newtonroot(testf1, 1.0)
     
-    @test abs(root1 - 1.0)<1e-6
-    @test abs(root2 - 0.0)<1e-6
+    @test root1 == 1.0
+    @test root2 < 1e-6
     
 
     #check maxiter works
-    #root1, conv, iter  = NewtonsMethod.newtonroot(testf, testf_prime, 0.5,  maxiter = 5)
-    # if the function converged, return number of iteration, else return nothing 
-   # @test iter == nothing || iter <= 5
-    #check for tolerance, increasing it to 1e-8
-    #root1, conv, iter  = NewtonsMethod.newtonroot(testf, testf_prime, x_0=0.5,  tol = 1e-8)
-    #@test abs(root1-1.0)<1e-7
+    root1, conv, iter  = NewtonsMethod.newtonroot(testf, testf_prime, 0.5,  maxiter = 5)
+    
+    @test iter == nothing || iter <= 5
+    #check for tolerance, increasing it to 1e-10
+    root1, conv, iter  = NewtonsMethod.newtonroot(testf, testf_prime, 0.5,  tol = 1e-10)
+    @test abs(root1-1.0) < 1e-7
     
     # Test no convergence
     
