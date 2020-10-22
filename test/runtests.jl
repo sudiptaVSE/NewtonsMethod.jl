9a67b7335a5aa60452aa8c2f002617004c097217
sudiptaVSE <71029731+sudiptaVSE@users.noreply.github.com>
2020-10-22 13:22:18 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,39 +9,39 @@ using Test
     
     #test for functions with derivative
 
-    root1, conv, iter  = NewtonsMethod.newtonroot(testf,testf_prime, 0.0)
-    root2, conv, iter  = NewtonsMethod.newtonroot(testf1,testf1_prime, 0.0)
+    root1, conv, iter  = NewtonsMethod.newtonroot(testf,testf_prime; x_0 = 0.0)
+    root2, conv, iter  = NewtonsMethod.newtonroot(testf1,testf1_prime; x_0 = 0.0)
     
     @test abs(root1 - 1.0)<1e-6
     @test abs(root2 - 0.0)<1e-6
     
     #do the same for functions without derivative
 
-    root1, conv, iter  = NewtonsMethod.newtonroot(testf, 0.0)
-    root2, conv, iter  = NewtonsMethod.newtonroot(testf1, 0.0)
+    root1, conv, iter  = NewtonsMethod.newtonroot(testf; x_0 = 0.0)
+    root2, conv, iter  = NewtonsMethod.newtonroot(testf1; x_0 = 0.0)
     
     @test abs(root1 - 1.0) < 1e-6
     @test abs(root2 - 0.0) < 1e-6
     
 
     #Test if maxiter works
-    root1, conv, iter  = NewtonsMethod.newtonroot(testfn,testfn_prime,x_0=0.0,  maxiter = 5)
+    root1, conv, iter  = NewtonsMethod.newtonroot(testf, testf_prime; x_0 = 0.0,  maxiter = 5)
     # if the function converged, return number of iteration, else return nothing 
     @test iter == nothing ||iter <= 5
     #Test different tolerance, increasing it to 1e-10
-    root1, conv, iter  = NewtonsMethod.newtonroot(testfn,testfn_prime,x_0=0.0,  tol = 1e-10)
+    root1, conv, iter  = NewtonsMethod.newtonroot(testf, testf_prime; x_0=0.0,  tol = 1e-10)
     @test abs(root1-1.0)<1e-7
     
     #Test lack of convergence
     
     test_nc(x) = x^2+1
 
-    rootnc, conv, iter  = NewtonsMethod.newtonroot(test_nc, 1.0)
+    rootnc, conv, iter  = NewtonsMethod.newtonroot(test_nc; x_0 = 1.0)
     @test rootnc==nothing
    
     x0 = BigFloat(0.79)
-    root1, conv, iter  = NewtonsMethod.newtonroot(testf, x0)
-    root2, conv, iter  = NewtonsMethod.newtonroot(testf1, x0)
+    root1, conv, iter  = NewtonsMethod.newtonroot(testf; x_0 = x0)
+    root2, conv, iter  = NewtonsMethod.newtonroot(testf1; x_0 = x0)
     
     @test abs(root1 - 1.0)<1e-6
     @test abs(root2 - 0.0)<1e-6
